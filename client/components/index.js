e8b0bf3d76518b483673be21ce0a56a74c74529e
18274774 <pvllebedev@sberbank.ru>
2020-04-24 18:56:08 +0300

    ц

--- client/components/index.js
+++ client/components/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import USERS from './USERS'
+// import USERS from './USERS'
 
 const Index = () => {
   return (
